cc91c9b6a81ec8f54c1cf1ea021e47d21971b8f0
EgorBarabash200 <yegor.barabash.97@mail.ru>
2025-06-01 03:37:53 +0300

    Всплывающее окно, сортировак select

--- tesr_react/src/App.js
+++ tesr_react/src/App.js
@@ -7,24 +7,40 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 function App() {
   const [post, setPost] = useState([
-    { id: 1, title: 'JavaScript 1', body: 'Description' },
-    { id: 2, title: 'JavaScript 2', body: 'Description' },
-    { id: 3, title: 'JavaScript 3', body: 'Description' }
+    { id: 1, title: 'AJavaScript 1', body: 'TDescription' },
+    { id: 2, title: 'MJavaScript 2', body: 'ADescription' },
+    { id: 3, title: 'DJavaScript 3', body: 'ZDescription' }
   ]);
-  const createPost = (newPost) =>{
-     setPost([...post, newPost]);
+  const [selectedSort, setSelectedSort] = useState('')
+  const createPost = (newPost) => {
+    setPost([...post, newPost]);
   }
-  const removePost = (posts) =>{
+  const removePost = (posts) => {
     setPost(post.filter(element => element.id !== posts.id))
+  }
+  const sortPost = (sort) =>{
+    setSelectedSort(sort)
+    setPost([...post].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
   return (
     <div className="App">
-      <PostForm create = {createPost}/>
+      <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0' }}></hr>
+      <MySelect
+        value={selectedSort}
+        onChange={sortPost}
+        defultValue='Сортировка'
+        options={[
+          { value: 'title', name: 'По названию' },
+          { value: 'body', name: 'По описанию' }
+        ]}
+      />
       {post.length !== 0
-        ? <PostList remove = {removePost} post={post} title='Список постов JavaScript' />
-        : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+        ? <PostList remove={removePost} post={post} title='Список постов JavaScript' />
+        : <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
       }
     </div>
   );
